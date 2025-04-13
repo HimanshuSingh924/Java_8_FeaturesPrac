30110e49dc20ce85abfaffbfb9f3a52e00684cb2
HimanshuSingh924 <himanshusingh00924@gmail.com>
2025-04-13 16:03:33 +0530

    check name start with A

--- src/main/java/hs/predicate/Main.java
+++ src/main/java/hs/predicate/Main.java
@@ -9,6 +9,10 @@ public class Main {
         Predicate<Integer> isEven = x -> x % 2 == 0; // here an condition store in
         System.out.println(isEven.test(18));              // variable : functional programming
 
+        Predicate<String> isStartWithA = str -> str.toLowerCase().startsWith("a");
+        String string = "Ankit";
+        System.out.println(isStartWithA.test(string));
+        System.out.println(isStartWithA.test(string) ? "Yes Start with A" : "No, Not Start with A");
 
 
     }
