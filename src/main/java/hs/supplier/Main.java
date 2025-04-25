7b8c8d5d9b2ee5248eaa447b060187e501b7b711
HimanshuSingh924 <himanshusingh00924@gmail.com>
2025-04-25 16:48:21 +0530

    Initial Commit: Creating new Project for practicing java 8 features Create an interface named by MathOperation.java as a function interface use of lambda in main class

--- src/main/java/hs/supplier/Main.java
+++ src/main/java/hs/supplier/Main.java
@@ -1,4 +1,12 @@
 package hs.supplier;
 
+import java.util.function.Supplier;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        Supplier<String> supplyString = () -> "Happy Boy!";
+        System.out.println(supplyString.get());
+    }
 }
